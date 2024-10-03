2f663b025399a0f062c0ddda1179bf18bacaeefd
Iqbal Tawakkal <iqbaltawakkal1@gmail.com>
2024-10-03 11:18:01 +0700

    refactor: register component and missing style in dev

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -13,8 +13,37 @@
 </template>
 
 <script setup>
-import { ref, watch } from 'vue';
+import './style.css';
+import { ref, watch, getCurrentInstance } from 'vue';
 import FormGenerator from './naive/FormGeneration.vue';
+import {
+    NForm,
+    NFormItem,
+    NInput,
+    NButton,
+    NSelect,
+    NDatePicker,
+    NAlert,
+    NSwitch,
+    NRadio,
+    NRadioGroup,
+    NCheckbox,
+    NCheckboxGroup,
+} from 'naive-ui';
+
+const app = getCurrentInstance().appContext.app;
+app.component('n-form', NForm);
+app.component('n-form-item', NFormItem);
+app.component('n-input', NInput);
+app.component('n-button', NButton);
+app.component('n-select', NSelect);
+app.component('n-date-picker', NDatePicker);
+app.component('n-alert', NAlert);
+app.component('n-switch', NSwitch);
+app.component('n-radio', NRadio);
+app.component('n-radio-group', NRadioGroup);
+app.component('n-checkbox', NCheckbox);
+app.component('n-checkbox-group', NCheckboxGroup);
 
 const props = defineProps({
     content: { type: Object, required: true },
@@ -25,7 +54,25 @@ const schema = {
     email: { type: 'email', label: 'Email', required: true },
     message: { type: 'textarea', label: 'Message' },
     date: { type: 'date', label: 'Date', required: true },
-    country: { type: 'select', label: 'Country', required: true, options: ['USA', 'Canada', 'UK', 'Germany'] },
+    switch: { type: 'switch', label: 'Switch' },
+    country: {
+        type: 'select',
+        label: 'Country',
+        required: true,
+        options: ['USA', 'Canada', 'UK', 'Germany'],
+    },
+    gender: {
+        type: 'radio',
+        label: 'Gender',
+        required: true,
+        options: ['Male', 'Female'],
+    },
+    toppings: {
+        type: 'checkbox',
+        label: 'Toppings',
+        required: true,
+        options: ['Cheese', 'Sausage', 'Lettuce'],
+    },
 };
 
 const updateComponent = ref(1);
